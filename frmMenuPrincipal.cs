a221eb7c16fc886978edf1e887e3565089081eee
Ale <83OT42647325@campus.economicas.uba.ar>
2022-11-12 22:38:46 -0300

    .

--- frmMenuPrincipal.cs
+++ frmMenuPrincipal.cs
@@ -5,18 +5,6 @@ namespace GRUPO_C
 {
     public partial class frmMenuPrincipal : Form
     {
-        // SOLICITUD DE SERVICIO
-        public static List<Int32> listaSolicitudesEnvio;
-        public static int ultimaSolicitud;
-
-        //ESTADO DE CUENTA
-        // CLAVE:VALOR -> Num de envio - Tarifa PARA EL PROTOTIPO
-        public static Dictionary<int, float> FacturasPagadas; // CLAVE:VALOR -> Num de envio - Tarifa PARA EL PROTOTIPO
-        public static Dictionary<int, float> FacturasPendientesPago;
-        public static Dictionary<int, float> EnviosPendientesFacturar;
-        public static float saldoTotal;
-        public static bool testPrimeraVez; // solo en prototipo
-
         // LOGIN
         List<ClienteCorporativo> listaClientesCorporativos = new List<ClienteCorporativo>();
         ClienteCorporativo cliente;
@@ -24,12 +12,6 @@ namespace GRUPO_C
 
         public frmMenuPrincipal()
         {
-            listaSolicitudesEnvio = new List<Int32>(); // ES LA LISTA DE SOLICITUDES DEL USUARIO QUE USA EL SISTEMA. PARA EL PROTOTIPO HAY UN USUARIO SOLO, POR ESO ES IGUAL EN TODO EL SISTEMA
-            ultimaSolicitud = 1; // Inicia el sistema, el numero de solicitud es 1
-            FacturasPagadas = new Dictionary<int, float>();
-            FacturasPendientesPago = new Dictionary<int, float>();
-            EnviosPendientesFacturar = new Dictionary<int, float>();
-            testPrimeraVez = true;
             InitializeComponent();
         }
 
@@ -131,13 +113,6 @@ namespace GRUPO_C
             SolicitudCancelacionEnvioMenu.Enabled = true;
             EstadoDelServicioMenu.Enabled = true;
             EstadoDeCuentaMenu.Enabled = true;
-        }
-
-        public void DeshabilitarMenu()
-        {
-            SolicitudCancelacionEnvioMenu.Enabled = false;
-            EstadoDelServicioMenu.Enabled = false;
-            EstadoDeCuentaMenu.Enabled = false;
         }
     }
 }
